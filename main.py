bccbf17c764d31ea6a3111b4b5b03678d9a2d906
blosher13@gmail.com <blosher13@gmail.com>
2025-09-13 13:15:41 -0700

    created classes

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import numpy as np
+from PIL import Image
+
 class Canvas:
     def __init__(self, bg_color, bg_height, bg_width):
         self.bg_color = bg_color
@@ -5,11 +8,33 @@ class Canvas:
         self.bg_width = bg_width
 
     def generate_canvas(self):
-        self.bg_color = input('')
-
+        background_image = np.zeros((self.bg_height, self.bg_width, 3), dtype=np.uint8)
+        if self.bg_color == 'white':
+            background_image[:] = [255, 255, 255]
+        elif self.bg_color == 'black':
+            background_image[:] = [0, 0, 0]
+        else:
+            print('invalid background color')
+        return background_image
+        
 class Shape:
-    def __init__(self):
-        pass
+    def __init__(self, x_coord, y_coord, r_color, g_color, b_color):
+        self.x_cord = x_coord
+        self.y_cord = y_coord
+        self.r_color = r_color
+        self.g_color = g_color
+        self.b_color = b_color
     
-    def paint_shape(self, x_coord, y_coord):
-        x_coord = input('')
+    def paint_shape(self, background_image):
+        background_image[self.x_cord:self.y_cord] = [self.r_color, self.g_color, self.b_color]
+        shape = Image.fromarray(background_image, 'RGB')
+        return background_image
+    
+class Photo:
+    def __init__(self, image_array):
+        self.image_array = image_array
+
+    def generate_image(self):
+        image = Image.fromarray(self.image_array, 'RGB')
+        return image
+

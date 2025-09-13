c3f2201c19ed4106255efe5741dbdeac3910642b
Blosher Brar <blosher13@gamil.com>
2025-09-13 10:11:06 -0700

    added empty classes

---
+++ main.py
@@ -0,0 +1,15 @@
+class Canvas:
+    def __init__(self, bg_color, bg_height, bg_width):
+        self.bg_color = bg_color
+        self.bg_height = bg_height
+        self.bg_width = bg_width
+
+    def generate_canvas(self):
+        self.bg_color = input('')
+
+class Shape:
+    def __init__(self):
+        pass
+    
+    def paint_shape(self, x_coord, y_coord):
+        x_coord = input('')
